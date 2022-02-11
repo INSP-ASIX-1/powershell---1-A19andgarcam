9b31365dcfab466297faaceb19d8c3b5773cf44e
A19andgarcam <95307570+A19andgarcam@users.noreply.github.com>
2022-02-11 19:40:23 +0100

    PowerShell - GitHub Classroom - 1_AndresGarcia

--- 03.ps1
+++ 03.ps1
@@ -1,2 +1,2 @@
 #Crea la carpeta myTest des de PowerShell, al mateix lloc on et trobes
-New-Item -path 'home/ausias/Escriptori/PowerShell' -ItemType Directory
+mkdir myTest
--- 04.ps1
+++ 04.ps1
@@ -1,2 +1,2 @@
 #Crea el fitxer test.txt des de PowerShell
- New-Item -path 'home/ausias/Escriptori/PowerShell/test.txt' -ItemType File
+touch test.txt
